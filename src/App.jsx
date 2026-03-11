6dc352b2e066a02c36bbbca527f1245221a4471a
Jennison Diniz <jennisonenthony@gmail.com>
2026-03-10 20:22:22 -0400

    boa noite

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+
+import './App.css'
+import Footer from './components/Footer'
+
+function App() {
+  
+
+  return (
+    <>
+     
+     
+      <p className="read-the-docs">
+        <h1>My Daily Habits</h1>
+        <p>Gerencie seus hábitos diários de forma simples e visual.</p>
+      </p>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
+
+
+
+
