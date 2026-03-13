53c010c2ab6b75daf19287bc8802e33c0fa10e78
Jennison Diniz <jennisonenthony@gmail.com>
2026-03-12 21:04:27 -0400

    boa tarde

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,16 @@ import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import BemVindo from'./components/BemVindo'
+import SecaoHabitos from'./components/SecaoHabitos'
+import HabitList from './components/HabitList'
 
 function App() {
+   const habits = [
+    {id: 1, titulo: 'Exercício', meta: 5, ativo: true, diasFeitos: 5},
+    {id: 2, titulo: 'Leitura', meta: 7, ativo: true, diasFeitos: 3},
+    {id: 3, titulo: 'Meditação', meta: 7, ativo: false, diasFeitos: 0},
+    {id: 4, titulo: 'Hidratação', meta:7, ativo: true, diasFeitos: 6},
+   ]
   
 
   return (
@@ -16,6 +24,9 @@ function App() {
         descricao='Construindo uma rotina melhor, um hábito por vez.'
         />
       <BemVindo nomeUsuario="turma iteam" totalHabitos={55}/>
+      <SecaoHabitos titulo="Meus Hábitos">
+        <HabitList habits={habits}/>
+      </SecaoHabitos>
       <Footer/>
       </div>
     </>
